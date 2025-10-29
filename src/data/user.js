05795851e77d3d087437198ffe9cd676f4b6574b
NuwanKeshara <nuwankyazh@gmail.com>
2025-10-30 00:53:38 +0200

    add cryptoStreamX project

--- src/data/user.js
+++ src/data/user.js
@@ -105,6 +105,20 @@ const INFO = {
 			linkText: "View Project",
 			link: "https://github.com/NuwanKeshara/Ad-Campaign-Dataflow",
 		},
+		{
+			title: "CryptoStreamX: Real-Time Crypto Trade Analytics Pipeline",
+			description:
+			<>CryptoStreamX is a fully streaming data pipeline that captures and processes live cryptocurrency trade data from Binance in real time. Built with Apache Kafka, the system ingests, buffers and distributes high throughput event streams. Kafka Streams and ksqlDB power on the fly transformations, aggregations and real-time analytics without external compute layers. Kafka Connect integrates seamlessly with ClickHouse, a high-performance columnar database, for ultra-fast storage and query execution on time-series trade data. This architecture delivers a scalable, fault-tolerant data flow from ingestion to analytics, enabling instant insights into trading activity, price movements and market patterns.<br /><br /> Tech stack: <b>Apache Kafka, ksqlDB, Kafka Streams, Kafka Connect, ClickHouse database, Docker, Python </b></>,
+				
+			logo: [
+				"/kafka.png",
+				"/Clickhouse.png",
+				"/ksqldb.svg",
+				"/docker.png"
+			],
+			linkText: "View Project",
+			link: "https://github.com/NuwanKeshara/CryptoStreamX",
+		},
 	],
 };
 
